0c08b3ea26830c3e00ca3989bcef142197ce59e2
fmillerwork <florian.miller.work@gmail.com>
2022-01-14 16:46:49 +0100

    Ajout premiers cas de test pour fizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,11 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    if(n%3 == 0 && n%5 == 0)
+        return "fizzbuzz"
+    if(n%3 == 0)
+        return "Fizz"
+    if(n%5 == 0)
+        return "Buzz"
+    return null
 }
 
 module.exports = fizzBuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,13 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    // TODO: Tester fizzbuzz()
+test('fizzBuzz return Fizz if n is divisible by 3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+})
+
+test('fizzBuzz return Fizz if n is divisible by 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+})
+
+test('fizzBuzz return Fizz if n is divisible by 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe("fizzbuzz");
 })
